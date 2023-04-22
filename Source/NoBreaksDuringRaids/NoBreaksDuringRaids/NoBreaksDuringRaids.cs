771c34abdb7c1fdd7f62aee3a14869b83c59d354
Mapc1 <marcocosta579@gmail.com>
2023-04-22 03:20:14 +0100

    Add harmony patch to remove mental breaks when any non colonist human pawns are in the world

--- Source/NoBreaksDuringRaids/NoBreaksDuringRaids/NoBreaksDuringRaids.cs
+++ Source/NoBreaksDuringRaids/NoBreaksDuringRaids/NoBreaksDuringRaids.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using HarmonyLib;
 using Verse;
 
 namespace NoBreaksDuringRaids
@@ -7,7 +9,27 @@ namespace NoBreaksDuringRaids
     {
         static NoBreaksDuringRaidsMod()
         {
-            Log.Message("Hello World!");
+            var harmony = new Harmony("NoBreaksDuringRaidsMod.Patch");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(Pawn))]
+    [HarmonyPatch(nameof(Pawn.InMentalState), MethodType.Getter)]
+    public static class PawnInMentalStatePatch
+    {
+        static bool Postfix(bool __result, Pawn __instance)
+        {
+            // FIXME: Change this if to check only for raiders ant make it more readable
+            if (__instance.Map is null || !__instance.RaceProps.Humanlike || __instance.Map.mapPawns.AllPawnsSpawned.FindAll(p => p.RaceProps.Humanlike).FindAll(p => !p.IsColonist).Count == 0) 
+                return __result;
+            
+            if (__instance.MentalState != null)
+            {
+                __instance.MentalState.RecoverFromState();
+                Log.Message($"{__instance.Name} has recovered from it's mental state");
+            }
+            return false;
         }
     }
 }
