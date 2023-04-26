62b6563c82891712db9a67c269a00eb595c45c89
Mapc1 <marcocosta579@gmail.com>
2023-04-26 02:05:09 +0100

    Remove downed enemies from list of enemies and skip the function if the target is downed

--- Source/NoBreaksDuringRaids/NoBreaksDuringRaids/NoBreaksDuringRaids.cs
+++ Source/NoBreaksDuringRaids/NoBreaksDuringRaids/NoBreaksDuringRaids.cs
@@ -21,18 +21,18 @@ namespace NoBreaksDuringRaids
     [HarmonyPatch(nameof(MentalBreaker.CanDoRandomMentalBreaks), MethodType.Getter)]
     public static class MentalBreaker_CanDoRandomMentalBreaks_Patch
     {
-        static bool Postfix(bool __result, Pawn ___pawn)
+        public static bool Postfix(bool __result, Pawn ___pawn)
         {
-            if (!___pawn.Spawned || !___pawn.RaceProps.Humanlike)
+            if (!___pawn.Spawned || !___pawn.RaceProps.Humanlike || ___pawn.Downed)
                 return __result;
             
             // If there are any pawns from an enemy faction on the same map then pawns cannot enter mental breaks
             // TODO: Still not sure if pawns should end their mental breaks when an enemy enters the map
             var factionsInMap = ___pawn.Map.mapPawns.AllPawnsSpawned
-                    .FindAll(p => p.RaceProps.Humanlike)
+                    .FindAll(p => p.RaceProps.Humanlike && !p.Downed)
                     .Select(p => p.Faction);
             var enemiesInMap = factionsInMap.Any(f => f.HostileTo(___pawn.Faction));
-            
+
             return !enemiesInMap && __result;
         }
     }
