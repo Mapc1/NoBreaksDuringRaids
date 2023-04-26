70163d7590c59a799597dfdb9e8d8eb8f61097df
Mapc1 <marcocosta579@gmail.com>
2023-04-26 16:24:07 +0100

    Refactor the code and rename harmony id

--- Source/NoBreaksDuringRaids/NoBreaksDuringRaids/NoBreaksDuringRaids.cs
+++ Source/NoBreaksDuringRaids/NoBreaksDuringRaids/NoBreaksDuringRaids.cs
@@ -7,34 +7,12 @@ using Verse.AI;
 
 namespace NoBreaksDuringRaids
 {
-    [StaticConstructorOnStartup]
-    public static class NoBreaksDuringRaidsMod
+    public class NoBreaksDuringRaidsMod : Mod
     {
-        static NoBreaksDuringRaidsMod()
+        public NoBreaksDuringRaidsMod(ModContentPack content) : base(content)
         {
-            var harmony = new Harmony("NoBreaksDuringRaidsMod.Patch");
+            var harmony = new Harmony("Mapc1.NoBreaksDuringRaidsMod");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
-
-    [HarmonyPatch(typeof(MentalBreaker))]
-    [HarmonyPatch(nameof(MentalBreaker.CanDoRandomMentalBreaks), MethodType.Getter)]
-    public static class MentalBreaker_CanDoRandomMentalBreaks_Patch
-    {
-        public static bool Postfix(bool __result, Pawn ___pawn)
-        {
-            if (!___pawn.Spawned || !___pawn.RaceProps.Humanlike || ___pawn.Downed)
-                return __result;
-            
-            // If there are any pawns from an enemy faction on the same map then pawns cannot enter mental breaks
-            // TODO: Still not sure if pawns should end their mental breaks when an enemy enters the map
-            var factionsInMap = ___pawn.Map.mapPawns.AllPawnsSpawned
-                    .FindAll(p => p.RaceProps.Humanlike && !p.Downed)
-                    .Select(p => p.Faction);
-            var enemiesInMap = factionsInMap.Any(f => f.HostileTo(___pawn.Faction));
-
-            return !enemiesInMap && __result;
-        }
-    }
-    
 }
---
+++ Source/NoBreaksDuringRaids/NoBreaksDuringRaids/Patches.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace NoBreaksDuringRaids
+{
+    [HarmonyPatch(typeof(MentalBreaker))]
+    [HarmonyPatch(nameof(MentalBreaker.CanDoRandomMentalBreaks), MethodType.Getter)]
+    public static class MentalBreaker_CanDoRandomMentalBreaks_Patch
+    {
+        public static bool Postfix(bool __result, Pawn ___pawn)
+        {
+            if (!___pawn.Spawned || !___pawn.RaceProps.Humanlike || ___pawn.Downed)
+                return __result;
+            
+            // If there are any pawns from an enemy faction on the same map then pawns cannot enter mental breaks
+            // TODO: Still not sure if pawns should end their mental breaks when an enemy enters the map
+            var factionsInMap = ___pawn.Map.mapPawns.AllPawnsSpawned
+                    .FindAll(p => p.RaceProps.Humanlike && !p.Downed)
+                    .Select(p => p.Faction);
+            var enemiesInMap = factionsInMap.Any(f => f.HostileTo(___pawn.Faction));
+
+            return !enemiesInMap && __result;
+        }
+    }
+}
