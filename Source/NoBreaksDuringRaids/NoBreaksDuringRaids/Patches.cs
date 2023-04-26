c4dc9484af84d65edc251987da3f34f690f82da6
Mapc1 <marcocosta579@gmail.com>
2023-04-26 18:26:22 +0100

    Add Settings and english language xml

--- Source/NoBreaksDuringRaids/NoBreaksDuringRaids/Patches.cs
+++ Source/NoBreaksDuringRaids/NoBreaksDuringRaids/Patches.cs
@@ -14,6 +14,16 @@ namespace NoBreaksDuringRaids
         {
             if (!___pawn.Spawned || !___pawn.RaceProps.Humanlike || ___pawn.Downed)
                 return __result;
+
+            // If the ParentFaction is null then we are at a neutral map tile so we are not home
+            bool inHomeTile = ___pawn.Map.ParentFaction?.IsPlayer ?? false;
+            
+            // If the mod is disabled in player settlements and the pawn is at a player settlement skip this function
+            if (!NoBreaksDuringRaidsMod.Settings.EnableHome && inHomeTile)
+                return __result;
+            // If the PawnsNeverBreakOutside is enabled and the pawn is outside a home settlement then they cannot break
+            if (NoBreaksDuringRaidsMod.Settings.PawnsNeverBreakOutside && !inHomeTile)
+                return false;
             
             // If there are any pawns from an enemy faction on the same map then pawns cannot enter mental breaks
             // TODO: Still not sure if pawns should end their mental breaks when an enemy enters the map
@@ -21,7 +31,7 @@ namespace NoBreaksDuringRaids
                     .FindAll(p => p.RaceProps.Humanlike && !p.Downed)
                     .Select(p => p.Faction);
             var enemiesInMap = factionsInMap.Any(f => f.HostileTo(___pawn.Faction));
-
+            
             return !enemiesInMap && __result;
         }
     }
